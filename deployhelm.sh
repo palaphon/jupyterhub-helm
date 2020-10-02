60c00dbbb242735fe88b3ee9d621961ea3d9bfec
Palaphon Chunpong <palaphon@iknowplus.co.th>
2020-10-02 11:04:12 +0700

    fix helm command

--- deployhelm.sh
+++ deployhelm.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #for helm version 3
 #on namespaces enterpris-gateway
-helm upgrade --install jupyterhub . --namespaces enterprise-gateway
+helm upgrade --install jupyterhub . --namespace enterprise-gateway
